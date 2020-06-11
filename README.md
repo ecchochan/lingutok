3536e1b9ec2dcf2cf6aee7c73d7323b129e30b70
ecchochan <ecchochan@gmail.com>
2020-06-11 11:23:19 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+**An LinguisticTokenizer for English**
---
+++ setup.py
@@ -0,0 +1,59 @@
+#define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
+from setuptools import setup, find_packages, Extension
+import glob
+import itertools
+import numpy
+import os
+
+try:
+    from Cython.Build import cythonize
+
+
+    MARISA_ROOT_DIR = "marisa-trie"
+    MARISA_SOURCE_DIR = os.path.join(MARISA_ROOT_DIR, "lib")
+    MARISA_INCLUDE_DIR = os.path.join(MARISA_ROOT_DIR, "include")
+    MARISA_FILES = [
+        "marisa/*.cc",
+        "marisa/grimoire.cc",
+        "marisa/grimoire/io/*.cc",
+        "marisa/grimoire/trie/*.cc",
+        "marisa/grimoire/vector/*.cc",
+    ]
+
+    MARISA_FILES[:] = itertools.chain(
+        *(glob.glob(os.path.join(MARISA_SOURCE_DIR, path))
+        for path in MARISA_FILES))
+
+
+    ext_modules = [
+        Extension("LinguisticTokenizer", [
+            "src/agent.cpp",
+            "src/base.cpp",
+            "src/iostream.cpp",
+            "src/key.cpp",
+            "src/keyset.cpp",
+            "src/tokenizer.cpp",
+            "src/query.cpp",
+            "src/std_iostream.cpp",
+            "src/trie.cpp"
+        ], include_dirs=[MARISA_INCLUDE_DIR]),
+    ]
+        
+except ImportError:
+    ext_modules = None
+
+setup(
+    name='LinguisticTokenizer',
+    version='0.0.1',
+    description='Linguistic Tokenizer',
+    libraries=[("libmarisa-trie", {
+        "sources": MARISA_FILES,
+        "include_dirs": [MARISA_SOURCE_DIR, MARISA_INCLUDE_DIR]
+    })],
+    ext_modules=ext_modules,
+    package_data={'LinguisticTokenizer': ['resources/*']}
+)
+
+
+
+
