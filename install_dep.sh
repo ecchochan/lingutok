a077a31149599e64c76db6ac6ada670442b1904d
ecchochan <ecchochan@gmail.com>
2020-07-19 15:20:07 +0800

    checkpoint

---
+++ install_dep.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+apt install autoconf libtool -y
+
+cd LinguisticTokenizer
+git submodule update --init --recursive
+
+cd marisa-trie
+autoreconf -i
+./configure
+make
+make install
+
+ln -s $PWD/lib/marisa/.libs/libmarisa.so /usr/lib/liblibmarisa-trie.so
+ln -s $PWD/lib/marisa/.libs/libmarisa.so.0 /usr/lib/liblibmarisa-trie.so.0
+ln -s $PWD/lib/marisa/.libs/libmarisa.so /usr/lib/liblibmarisa.so
+ln -s $PWD/lib/marisa/.libs/libmarisa.so.0 /usr/lib/liblibmarisa.so.0
+ln -s $PWD/lib/marisa/.libs/libmarisa.so /usr/lib/libmarisa.so
+ln -s $PWD/lib/marisa/.libs/libmarisa.so.0 /usr/lib/libmarisa.so.0
+export LD_LIBRARY_PATH=/usr/lib:$LD_LIBRARY_PATH
+
+cd ..
+
+./update_cpp.sh
+
