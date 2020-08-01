7b40f5757496d81b8ff762eea5eb119a38d1607c
ecchochan <ecchochan@gmail.com>
2020-08-01 16:24:26 +0800

    update

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ import os
 import sys
 import subprocess
 import numpy
+subprocess.call(sys.executable + ' -m pip install cython', shell=True)
 
 subprocess.call("sudo bash ./install_dep.sh", shell=True)
 subprocess.call("sudo bash ./update_cpp.sh", shell=True)
